76452148b716aac5c30e4b4eca8b6cc3135bc0bf
CoreLogic Technology <musunguwarren1@gmail.com>
2026-06-15 14:11:20 +0300

    Update owner number, name, and timezone settings

--- settings.js
+++ settings.js
@@ -24,17 +24,17 @@ global.botname = process.env.BOT_NAME || 'KANGO-XMD'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '254102776096' 
 
 //--------------------[ SUDO ]--------------------------//
 
-global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['233509977126', '233577860202'];
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['254705127804', '254102776096'];
 // Type additional allowed users here
 //NB: They'll be able to use every functions of the bot without restrictions.
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'warren' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
@@ -43,7 +43,7 @@ global.packname = process.env.STICKER_PACK_NAME || "KANGO-XMD"
 //--------------[ COUNTRY TIMEZONE ]------------//
 
 
-global.timezones = 'Africa/Accra';  // Set this to you timezone
+global.timezones = 'Africa/Nairobi';  // Set this to you timezone
 
 
 
